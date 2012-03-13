efee5cf6fd3705736ad68a8eec65b88b671a1090
Benedikt Deicke <benedikt@synatic.net>
2012-03-13 13:47:07 +0100

    Fixes paperclip fallback to work with indifferent hash keys

--- lib/concealer/fallback/paperclip.rb
+++ lib/concealer/fallback/paperclip.rb
@@ -1,5 +1,6 @@
 class Concealer::Fallback::Paperclip
   def call(model, method, args)
-    ::Paperclip::Attachment.new(method, model.class.new, model.class.attachment_definitions[method])
+    attachment_definitions = model.class.attachment_definitions.with_indifferent_access
+    ::Paperclip::Attachment.new(method, model.class.new, attachment_definitions[method])
   end
 end
