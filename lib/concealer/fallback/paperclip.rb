ea1d60dbc73b3c0d34fdffb2f3e59ea0f5a3266b
Benedikt Deicke <benedikt@synatic.net>
2012-03-13 16:24:09 +0100

    Make sure the options get passed as symbols

--- lib/concealer/fallback/paperclip.rb
+++ lib/concealer/fallback/paperclip.rb
@@ -3,6 +3,6 @@ require 'active_support/core_ext/hash'
 class Concealer::Fallback::Paperclip
   def call(model, method, args)
     attachment_definitions = model.class.attachment_definitions.with_indifferent_access
-    ::Paperclip::Attachment.new(method, model.class.new, attachment_definitions[method])
+    ::Paperclip::Attachment.new(method, model.class.new, attachment_definitions[method].symbolize_keys)
   end
 end
--- spec/concealer/fallback/paperclip_spec.rb
+++ spec/concealer/fallback/paperclip_spec.rb
@@ -3,8 +3,8 @@ require 'spec_helper'
 describe Concealer::Fallback::Paperclip do
   describe '#call' do
     it "should return a paperclip attachment" do
-      ConcealedModel.stub!(:attachment_definitions).and_return({ :avatar => :something })
-      Paperclip::Attachment.should_receive(:new).with(:avatar, anything, :something).and_return(:paperclip)
+      ConcealedModel.stub!(:attachment_definitions).and_return({ :avatar => { :something => true } })
+      Paperclip::Attachment.should_receive(:new).with(:avatar, anything, :something => true).and_return(:paperclip)
       subject.call(ConcealedModel.new, :avatar, nil).should eq(:paperclip)
     end
   end
