b02bc1fbffc73ce641cfc5b389d9252cc7b6826d
Benedikt Deicke <benedikt@synatic.net>
2012-03-13 13:52:17 +0100

    Adds hash extensions to paperclip fallback

--- lib/concealer/fallback/paperclip.rb
+++ lib/concealer/fallback/paperclip.rb
@@ -1,3 +1,5 @@
+require 'active_support/core_ext/hash'
+
 class Concealer::Fallback::Paperclip
   def call(model, method, args)
     attachment_definitions = model.class.attachment_definitions.with_indifferent_access
